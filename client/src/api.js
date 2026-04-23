342d989b57a5f1dc82d423e50f3cf03eaa65bd81
SAHIL <154042482+Sahil-Khoja@users.noreply.github.com>
2026-04-23 14:59:22 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://github.com/Sahil-Khoja/to-do-app/api",
 });
 
 export default API;
